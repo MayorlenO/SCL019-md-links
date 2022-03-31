8c156a46ac5cc2c333cfced56a7f6aa03051440a
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-03-31 00:39:05 -0300

    path verification code iteration

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-const verifyExistence = (resp)  => fs.existsSync(resp);
-    
+const verifyExistence = (resp)  => fs.existsSync(resp); 
 
 const verifyExtension = (resp) => path.extname(resp) === '.md';
-        
-  
-const verifyPath = (resp) => path.isAbsolute(resp);
-       
- 
-const AbsoluteTransform = (resp) => path.resolve(resp);
 
-  exports.verifyExtension = verifyExtension;
-  exports.verifyExistence = verifyExistence;
-  exports.verifyPath = verifyPath;
-  exports.AbsoluteTransform = AbsoluteTransform;
+const isFile = (resp) => fs.statSync(resp).isFile()
+
+const isDirectory = (resp) => fs.lstatSync(resp).isDirectory()
+
+
+exports.verifyExtension = verifyExtension;
+exports.verifyExistence = verifyExistence;
+exports.isFile = isFile;
+exports.isDirectory = isDirectory;
+  
 
