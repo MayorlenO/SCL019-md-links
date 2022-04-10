3eb075f85fcf9c03292fe212787c384754353787
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-04-10 00:15:48 -0400

    update chalks

--- api.js
+++ api.js
@@ -6,7 +6,6 @@ import {
   verifyExtension,
   isFile,
   isDirectory,
-  readFile,
   dirOMd,
   validateOpt,
 } from "./index.js";
@@ -36,22 +35,20 @@ process.stdin.on("data", function (data) {
     process.stdout.write(
       chalk.bgRedBright(
         chalk.black(
-          "El archivo o directorio no existe. Verifique y vuelva a iniciar la librería"
-        )
-      )
-    );
-    exit();
+          "El archivo o directorio no existe ")))
+          process.stdout.write(chalk.green(' Vuelva a iniciar y pruebe con otra ruta:'))
+            exit();
   }
 
   if (verifyExistence(route) && isDirectory(route)) {
-    process.stdout.write("El directorio existe \n");
+    process.stdout.write(chalk.bgWhite(chalk.black("El directorio existe! \n")));
     process.stdout.write("Transformando la ruta a absoluta: \n");
     process.stdout.write(ruta + "\n");
-    process.stdout.write("Los archivos del directorio son: \n");
+    process.stdout.write(chalk.green("Los archivos del directorio son: \n"));
 
     let dirData = new Array();
     dirData = fs.readdirSync(ruta);
-    console.log(dirData);
+    console.log(chalk.bgGrey(chalk.white(dirData)));
     process.stdout.write(" \n Y se han encontrado estos archivos .md \n");
 
     const filesMd = [];
@@ -61,9 +58,9 @@ process.stdin.on("data", function (data) {
       }
     }
 
-    console.log(filesMd);
+    console.log(chalk.bgRed(chalk.white(filesMd)));
     process.stdout.write("\n");
-    process.stdout.write("Ingrese la ruta del archivo que desea revisar \n");
+    process.stdout.write("Ingrese la ruta del archivo .md que desea revisar \n");
   }
 
   if (isFile(ruta) && verifyExtension(ruta)) {
@@ -81,10 +78,10 @@ process.stdin.on("data", function (data) {
             resolve(validateOpt(totalLinks)).then((r) => console.log(r));
           }
         } else {
-          reject(new Error("No se ha encontrado ningún link"));
+          reject(new Error("No se ha encontrado ningún link. Pruebe con otro archivo:"));
         }
       }).catch((err) => {
-        console.log(chalk.red("Este es el " + err));
+        console.log(chalk.bgRed(chalk.black(err)));
       });
     };
 
@@ -93,9 +90,9 @@ process.stdin.on("data", function (data) {
     });
   } else if (isFile(ruta) && !verifyExtension(ruta)) {
     process.stdout.write(
-      chalk.bgRedBright(chalk.black("El archivo no es MD!"))
+      chalk.bgRedBright(chalk.black("El archivo no es MD!. Ingrese una ruta válida: \n"))
     );
-    exit();
+    
   }
 });
 
