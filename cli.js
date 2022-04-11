6569c469a50afd9b9cbb3c1621998212f8318299
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-04-11 08:36:31 -0400

    Modify and update all functions

--- cli.js
+++ cli.js
@@ -12,7 +12,55 @@ import {
   import util from "util";
   import chalk from "chalk";
   
+  const messageStart = () => {
+    const commands = [
+      {
+        option: '',
+        structure: 'md-links <path-to-file>',
+        example: './some/example.md',
+        outpout: '[{ file, href, text }]',
+      },
+      {
+        option: '--validate',
+        structure: 'md-links <path-to-file> [options]',
+        example: './some/example.md --validate',
+        outpout: '[{ file, href, message, status, text }]',
+      },
+      {
+        option: '--stats',
+        structure: 'md-links <path-to-file> [options]',
+        example: './some/example.md --stats',
+        outpout: '[{ total, unique }]',
+      },
+      {
+        option: '--stats --validate',
+        structure: 'md-links <path-to-file> [options]',
+        example: './some/example.md --stats --validate',
+        outpout: '[{ total, unique, broken }]',
+      },
+      {
+        option: '--help',
+        structure: '',
+        example: 'md-links <path-to-file> [options]',
+        outpout: 'Volver a mostrar las opciones',
+      },
+    ]
+
+    console.table(
+      commands.map(command => {
+        return {
+          Opciones: command.option,
+          Ejemplo: command.example,
+          Outpout: command.outpout,
+        }
+      })
+    )
+  }
+
+
+
    let route = "";
+   let opt;
   
   process.stdout.write(
     chalk.green("Ingrese la ruta del archivo / directorio que desea revisar:\n")
@@ -72,20 +120,82 @@ import {
           chalk.bgRedBright(chalk.black("El archivo no es MD!. Ingrese una ruta válida: \n"))
         );
     }
-
-let options = {
-    validate: false,
-};
-
-if (
-    (ruta === '--validate' ) 
-) {
-    options.validate = true;
-} 
-
-mdLinks(ruta, options)
-    .then(file => {
-    console.log(file);
+  })
+  
+  if (opt === undefined) {
+    mdLinks(args, {
+      validate: false,
     })
-    .catch(err => console.log('error', err));
-  });
+      .then(result => {
+        if (result.length > 0) {
+          return result.forEach(element =>
+            console.log(
+              `File => ${chalk.blue(element.file)}, Url => ${chalk.yellow(element.href)}, Text => ${chalk.cyan(
+                element.text
+              )}`
+            )
+          )
+        }
+        messageNoMd(args)
+      })
+      .catch(error => messageNoExist(error,args))
+  } else if (opt === '--validate') {
+    mdLinks(args, {
+      validate: true,
+    })
+      .then(result => {
+        if (result.length > 0) {
+          return result.forEach(function (element) {
+            if (element.status >= 400 || element.status == 'Error') {
+              console.log(
+                `File => ${chalk.blue(element.file)}, Status => ${chalk.red(
+                  element.textStatus,
+                  element.status
+                )}, Url => ${chalk.red(element.href)},Text => ${chalk.cyan(element.text)}`
+              )
+            } else {
+              console.log(
+                `File => ${chalk.blue(element.file)}, Status => ${chalk.green(
+                  element.textStatus,
+                  element.status
+                )}, Url => ${chalk.yellow(element.href)}, Text => ${chalk.cyan(element.text)}`
+              )
+            }
+          })
+        }
+        messageNoMd(args)
+      })
+      .catch(error => messageNoExist(error,args))
+  } else if (opt === '--stats') {
+    mdLinks(args, {
+      validate: true,
+    })
+      .then(result => {
+        if (result.length > 0) {
+          return console.log(`Total => ${result.length} \nUnique => ${uniqueLinks(result)}`)
+        }
+        messageNoMd(args)
+      })
+      .catch(error => messageNoExist(error,args))
+  } else if (opt === '--stats --validate') {
+    mdLinks(args, {
+      validate: true,
+    })
+      .then(result => {
+        if (result.length > 0) {
+          
+          return console.log(
+            `Total => ${result.length} \nUnique => ${uniqueLinks(result)} \nBroken => ${brokenLinks(result).length} \nSon los siguientes:`, brokenLinks(result)
+          )
+        }
+  
+        messageNoMd(args)
+      })
+      .catch(error => messageNoExist(error, args))
+  } else if (opt === '--help') {
+    messageStart()
+  } else {
+    console.log(chalk.bold.bgRed(`La opción ${opt} es inválida`))
+    messageStart()
+  }
+  
--- index.js
+++ index.js
@@ -3,118 +3,145 @@ import fs from 'fs';
 import fetch from 'node-fetch';
 
 
+const __dirname = path.resolve('')
+
 export const verifyExistence = (ruta)  => fs.existsSync(ruta); 
 
-export const verifyExtension = (ruta) => path.extname(ruta) === '.md';
+export const isAbsolutePath = route => path.isAbsolute(route)
 
-export const isFile = (ruta) => fs.statSync(ruta).isFile()
-
-export const isDirectory = (ruta) => fs.lstatSync(ruta).isDirectory()
-
-export const linksMd = (file, files) => {
-    const line = file.split('\n');// separa en lineas el documento
-    let linksArray = [];
-    
-    for ( let i=0; line.length > i; i++) {
-      const lineI = line[i];
-      const reguExpress = /\[([^\]]+)]\((https?:\/\/[^\s)]+)\)/g; // expresión regular que muestra el texto y los links
-      const matchLinks = lineI.matchAll(reguExpress);// busca coincidencias
-      const testMatch = reguExpress.test(lineI); // true o false
-      if(testMatch) {
-        for(const match of matchLinks) {
-          var objectLink = {
-            href: match[2],
-            text: match[1],
-            file: files,
-            line: i + 1,
-          }
-        }linksArray.push(objectLink);
-      }
-     console.log('\n' + 'Se encontró '+ linksArray.length +' link(s) en el archivo .md')
-    }return linksArray;
-    }
-  
-
-  export const readFile = (files, mdLinks) => {
-    const file = fs.readFileSync(files, 'utf8');//lee el archivo y lo devuelve
-    mdLinks.push(...linksMd(file, files)); // spread operator
-};
-
-  export const md = (routeTotal, totalLinks) => {
-    if(verifyExtension(routeTotal)) {
-      readFile(routeTotal, totalLinks);
-    }
-  };
-  
-  export const validateOpt = (linksArray) => {
-    const statusLink = linksArray.map((obj) =>
-      fetch(obj.href)
-      .then((res) => {
-        if (res.status === 200) {
-          return {
-            href: obj.href,
-            text: obj.text,
-            file: obj.file,
-            status: res.status,
-            statusText: 'Ok',
-          };
-        } else {
-          return {
-            href: obj.href,
-            text: obj.text,
-            file: obj.file,
-            status: res.status,
-            statusText: 'Fail',
-          };
-          
-        }
-      })
-      .catch((err) =>
-        ({
-          href: obj.href,
-          text: obj.text,
-          file: obj.file,
-          status: 404,
-          statusText: 'Fail',
-        }),
-      ));
-    return Promise.all(statusLink);
-  };
-  
-  export const statsArray = (validOpt) => {
-    let objStats = {}
-    objStats.Total = validateOpt.length;
-    objStats.Unique = 0; 
-    const uniqueLinks = new Set(); // Objeto que permite almacenar valores únicos de cualquier tipo
-    validOpt.forEach(obj => {
-    uniqueLinks.add(obj.href);
-    });
-    objStats.Unique = uniqueLinks.size; // devuelve el número de elementos que hay en el objeto Set.
-    return objStats;
-};
+export const convertToAbsolute = route => path.join(__dirname, route)
 
 
-// función validar y status 
-export const validateStats = (arrayLinks) => {
-    return Promise.resolve(
-validateArray(arrayLinks)
-    .then(validatedArr=>{
-        let objeValStat = {};
-        objeValStat.Total = validatedArr.length;
-        objeValStat.Unique = 0;
-        objeValStat.Broken = 0;
-        const uniqueLinks = new Set();
-        validatedArr.forEach(obj => {
-            uniqueLinks.add(obj.href);
-            if (obj.status === 404) {
-                objeValStat.Broken += 1;
-            }
-        });
-        objeValStat.Unique = uniqueLinks.size;
-        return objeValStat;
-    })
-    .catch(err=>console.log('Error de validación' + err)))
+export const isFile = route => {
+  const stat = fs.lstatSync(route)
+  const result = stat.isFile()
+  return result
 }
+
+export const IsDirectory = route => {
+  let arrayFiles = []
+  if (isFilePath(route)) {
+    arrayFiles.push(route)
+  } else {
+    const readDirectory = fs.readdirSync(route)
+    readDirectory.forEach(file => {
+      const pathFile = path.join(route, file)
+      arrayFiles.push(pathFile)
+    })
+  }
+  return arrayFiles
+}
+
+export const verifyExtension = arrayFiles => {
+  return arrayFiles.filter(file => {
+    return path.extname(file) === '.md'
+  })
+}
+
+// export const linksMd = (file, files) => {
+//     const line = file.split('\n');// separa en lineas el documento
+//     let linksArray = [];
+    
+//     for ( let i=0; line.length > i; i++) {
+//       const lineI = line[i];
+//       const reguExpress = /\[([^\]]+)]\((https?:\/\/[^\s)]+)\)/g; // expresión regular que muestra el texto y los links
+//       const matchLinks = lineI.matchAll(reguExpress);// busca coincidencias
+//       const testMatch = reguExpress.test(lineI); // true o false
+//       if(testMatch) {
+//         for(const match of matchLinks) {
+//           var objectLink = {
+//             href: match[2],
+//             text: match[1],
+//             file: files,
+//             line: i + 1,
+//           }
+//         }linksArray.push(objectLink);
+//       }
+//      console.log('\n' + 'Se encontró '+ linksArray.length +' link(s) en el archivo .md')
+//     }return linksArray;
+//     }
+  
+
+//   export const readFile = (files, mdLinks) => {
+//     const file = fs.readFileSync(files, 'utf8');//lee el archivo y lo devuelve
+//     mdLinks.push(...linksMd(file, files)); // spread operator
+// };
+
+//   export const md = (routeTotal, totalLinks) => {
+//     if(verifyExtension(routeTotal)) {
+//       readFile(routeTotal, totalLinks);
+//     }
+//   };
+  
+//   export const validateOpt = (linksArray) => {
+//     const statusLink = linksArray.map((obj) =>
+//       fetch(obj.href)
+//       .then((res) => {
+//         if (res.status === 200) {
+//           return {
+//             href: obj.href,
+//             text: obj.text,
+//             file: obj.file,
+//             status: res.status,
+//             statusText: 'Ok',
+//           };
+//         } else {
+//           return {
+//             href: obj.href,
+//             text: obj.text,
+//             file: obj.file,
+//             status: res.status,
+//             statusText: 'Fail',
+//           };
+          
+//         }
+//       })
+//       .catch((err) =>
+//         ({
+//           href: obj.href,
+//           text: obj.text,
+//           file: obj.file,
+//           status: 404,
+//           statusText: 'Fail',
+//         }),
+//       ));
+//     return Promise.all(statusLink);
+//   };
+  
+//   export const statsArray = (validOpt) => {
+//     let objStats = {}
+//     objStats.Total = validateOpt.length;
+//     objStats.Unique = 0; 
+//     const uniqueLinks = new Set(); // Objeto que permite almacenar valores únicos de cualquier tipo
+//     validOpt.forEach(obj => {
+//     uniqueLinks.add(obj.href);
+//     });
+//     objStats.Unique = uniqueLinks.size; // devuelve el número de elementos que hay en el objeto Set.
+//     return objStats;
+// };
+
+
+// // función validar y status 
+// export const validateStats = (arrayLinks) => {
+//     return Promise.resolve(
+// validateArray(arrayLinks)
+//     .then(validatedArr=>{
+//         let objeValStat = {};
+//         objeValStat.Total = validatedArr.length;
+//         objeValStat.Unique = 0;
+//         objeValStat.Broken = 0;
+//         const uniqueLinks = new Set();
+//         validatedArr.forEach(obj => {
+//             uniqueLinks.add(obj.href);
+//             if (obj.status === 404) {
+//                 objeValStat.Broken += 1;
+//             }
+//         });
+//         objeValStat.Unique = uniqueLinks.size;
+//         return objeValStat;
+//     })
+//     .catch(err=>console.log('Error de validación' + err)))
+// }
 
 
 
